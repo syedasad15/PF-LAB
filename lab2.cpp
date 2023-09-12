8293cdd687ec2ebf8fab5fe1711bea9b7703bf6b
syedasad15 <144409006+syedasad15@users.noreply.github.com>
2023-09-11 23:33:26 -0700

    Add files via upload

---
+++ lab2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"           *********************\n";
+cout<<"          *                   ** \n";
+cout<<"         *                   * *\n";
+cout<<"        *                   *  * \n";
+cout<<"       *                   *   * \n";
+cout<<"      *********************    *   \n";
+cout<<"      *                   *    *       \n";
+cout<<"      *                   *    *       \n";
+cout<<"      *                   *    *       \n";
+cout<<"      *                   *    *       \n";
+cout<<"      *                   *    *       \n";
+cout<<"      *                   *    *       \n";
+cout<<"      *                   *    *        \n";
+cout<<"      *       UET         *    *       \n";
+cout<<"      *                   *    *       \n";
+cout<<"      *                   *    *       \n";
+cout<<"      *                   *    *       \n";
+cout<<"      *                   *    *       \n";
+cout<<"      *                   *    *       \n";
+cout<<"      *                   *    *       \n";
+cout<<"      *                   *  *       \n";
+cout<<"      *********************      \n";
+}
